c8b856945fa58fa57734647e5c843404653e88f6
dina-fouad <dina_albarghouthi@yahoo.com>
2021-09-29 17:12:38 +0300

    feat:create product test

--- base/tests.py
+++ base/tests.py
@@ -25,3 +25,32 @@ class AllProductTests(APITestCase):
         url = reverse('orders')
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)      
+
+
+class Test_Create_Product(TestCase):
+
+
+    @classmethod
+    def setUpTestData(cls):
+        
+        testuser1 = User.objects.create(
+            username = "testuser1" , password='123456789'
+        )
+        test_product=Product.objects.create(name="hp2020"  , brand="hp" ,category="laptop" , description="good" , price=800)   
+
+    def test_product_content(self):
+        product = Product.productobjects.get(_id=1)
+
+        name=f'{product.name}'
+       
+        brand=f'{product.brand}'
+        category=f'{product.category}'
+        description=f'{product.description}'
+        price=f'{product.price}'
+
+        self.assertEqual(name , "hp2020")
+        
+        self.assertEqual(brand , "hp")
+        self.assertEqual(category , "laptop")
+        self.assertEqual(description , "good")
+        self.assertEqual(price , "800.00")
